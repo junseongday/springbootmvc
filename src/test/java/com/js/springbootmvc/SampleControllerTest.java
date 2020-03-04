81a6be1f5d7ea8b2171c3820f4f43ed08aff5713
junseongday <junseongday@gmail.com>
2020-03-04 20:53:06 +0900

    Initial commit

---
+++ src/test/java/com/js/springbootmvc/SampleControllerTest.java
@@ -0,0 +1,27 @@
+package com.js.springbootmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void hello() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("junseong")));
+    }
+}
